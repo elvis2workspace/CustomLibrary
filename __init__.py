aa4ac6920b03950b673db16cee9a014561c9cd68
Elvis Zhang <xiuhai5052@hotmail.com>
2016-06-23 15:54:40 +0800

    improve __init__.py script.

--- __init__.py
+++ __init__.py
@@ -2,13 +2,12 @@
 
 import os
 from customkeywords import *
-from customversion import CUSTOMLIBVERSION
+from customversion import CUSTOM_LIB_VERSION
 
 THIS_DIR = os.path.dirname(os.path.abspath(__file__))
- 
 execfile(os.path.join(THIS_DIR, 'customversion.py'))
 
-__version__ = CUSTOMLIBVERSION
+__version__ = CUSTOM_LIB_VERSION
 
 
 class CustomLibrary(
@@ -16,15 +15,16 @@ class CustomLibrary(
         _NativeOperationKeywords,
         _OutputResultKeywords,
         _OutputRunnerKeywords,
-        _OutputPwdKeywords,):
+        _OutputPwdKeywords,
+):
 
     """
     这里也可以装x 的写上我们创建的CustomLibrary 如何如何。
     """
+
     ROBOT_LIBRARY_SCOPE = 'GLOBAL'
-    ROBOT_LIBRARY_VERSION = CUSTOMLIBVERSION
+    ROBOT_LIBRARY_VERSION = CUSTOM_LIB_VERSION
     
     def __init__(self):
-        
         for base in CustomLibrary.__bases__:
             base.__init__(self)
