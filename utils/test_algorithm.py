a7c530341e4c8148bb53af66df99b9b2220d0217
Elvis Zhang <xiuhai5052@hotmail.com>
2016-09-01 09:18:05 +0800

    add test_algorithm.py and test_monkey.py

---
+++ utils/test_algorithm.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+
+"""
+确定一个字符串中是否所有字符全部互不相同
+"""
+
+from collections import Counter
+
+
+# 将字符串中的每一个字符与剩下的字符比较，如果遇到相同的元素，则返回False，如果直到遍历结束都没有遇到相同元素，则返回True：
+def is_unique_char(string):
+    str_len = len(string)
+
+    if str_len > 256:
+        return True
+
+    for pos in xrange(str_len):
+        for index in xrange(pos+1, str_len):
+            if string[pos] == string [index]:
+                return False
+
+    return True  # 时间复杂度为O(n*n)，空间复杂度为O(1)。这种解法的效率非常低下
+
+
+# 构建一个布尔值的数组，索引index表示ASCII码中值为index的字符。将初值置为False，如果某个元素第二次出现，
+# 则表示这个字符串出现了重复的字符，函数直接返回
+def is_unique_char1(string):
+    if len(string) > 256:
+        return True
+
+    record = [False] * 256
+
+    for ch in string:
+        ch_val = ord(ch)
+        if record[ch_val]:
+            return False
+        record[ch_val] = True
+
+    return True  # 时间复杂度为O(n)，空间复杂度为O(1)。不过，我们可以非常确定的是，n的最大值仅仅为256。
+
+
+# 使用位运算，结合Python中数字的特殊实现，我们仅需要一个数字来替代record即可实现上面的算法
+def is_unique_char2(string):
+    if len(string) > 256:
+        return True
+
+    record = 0L
+
+    for ch in string:
+        print record
+        ch_val = ord(ch)
+
+        if (record & (1 << ch_val)) > 0:
+            return False
+        record |= (1 << ch_val)
+
+    return True
+
+
+# Python的某些数据结构，则我们可以通过collections里的工具来实现：
+is_unique_char3 = lambda s: True if len(s) > 256 else not bool(filter(lambda n: n > 1,Counter(s).values()))
+
+
+# 对字符串进行排序后，然后遍历每一个元素并与周围元素比较(请自行尝试)
+
+
+
+if __name__ == '__main__':
+    string_tmp = 'zfleehfdfhefnlaflhiefldfhilllddez'
+    print "is_unique_char: ", is_unique_char(string_tmp)
+    print "is_unique_char1: ", is_unique_char1(string_tmp)
+    print "is_unique_char2: ", is_unique_char2(string_tmp)
+    print "isis_unique_char3: ", is_unique_char3(string_tmp)
---
+++ utils/test_monkey.py
@@ -0,0 +1,114 @@
+# -*- coding: utf-8 -*-
+
+import os
+import time
+import re
+
+packageName = "com.XXXX.app"
+log_dir = r"d:\jenkins"
+remote_path = r"\\10.21.101.100\build\android"
+os.system('adb shell cat /system/build.prop > D:\jenkins\phone.text')
+file_s = r"D:\jenkins\phone.text"
+
+NullPointer = "java.lang.NullPointerException"
+IllegalState = "java.lang.IllegalStateException"
+IllegalArgument = "java.lang.IllegalArgumentException"
+ArrayIndexOutOfBounds = "java.lang.ArrayIndexOutOfBoundsException"
+RuntimeException = "java.lang.RuntimeException"
+SecurityException = "java.lang.SecurityException"
+
+
+def getcmd(file_p):
+    file_tmp = open(file_p, "r")
+    lines = file_tmp.readlines()
+    for line in lines:
+        line = line.split('=')
+
+    if line[0] == 'ro.build.version.release':
+        versions = line[1]
+    if line[0] == 'ro.product.model':
+        models = line[1]
+    if line[0] == 'ro.product.brand':
+        brands = line[1]
+
+    return versions, models, brands
+
+version, model, brand = getcmd(file_s)
+print version, model, brand
+os.remove(file_s)
+
+# print "使用Logcat清空Phone中log"
+os.popen("adb logcat -c")
+# print"暂停2秒..."
+print "wait"
+time.sleep(2)
+
+now1 = time.strftime('%Y-%m-%d-%H_%M_%S', time.localtime(time.time()))
+
+# print"开始执行Monkey命令"
+monkey_log = log_dir + "\\" + now1 + "monkey.log"
+print monkey_log
+cmd = "adb shell monkey -p com.XXXX -s 500 --ignore-timeouts --monitor-native-crashes -v -v 10000 >> %s" % monkey_log
+os.popen(cmd)
+
+# print"手机截屏"
+os.popen("adb shell screencap -p /sdcard/monkey_run.png")
+
+# print"拷贝截屏图片至电脑"
+cmd1 = "adb pull /sdcard/monkey_run.png %s" % log_dir
+os.popen(cmd1)
+
+print "gai ming"
+old_name = log_dir+"\\"+r"monkey_run.png"
+if os.path.exists(old_name):
+    print "file is exist"
+else:
+    print "file isn't exist"
+new_name = log_dir+"\\" + now1 + r"monkey.png"
+os.rename(old_name, new_name)
+
+# print"使用Logcat导出日志"
+logcat_name = log_dir+"\\" + now1 + r"logcat.log"
+cmd2 = "adb logcat -d >%s" % logcat_name
+os.popen(cmd2)
+
+# print"导出traces文件"
+traces_name = log_dir+"\\"+now1+r"traces.log"
+cmd3 = "adb shell cat /data/anr/traces.txt>%s" % traces_name
+os.popen(cmd3)
+
+
+def get_error(logcat_name_file):
+    f = open(logcat_name_file, "r")
+    lines = f.readlines()
+    err_file = "%s\error.log" % remote_path
+    if os.path.exists(err_file):
+        os.remove(err_file)
+
+    fr = open(err_file, "a")
+    fr.write(version)
+    fr.write("\n")
+    fr.write(model)
+    fr.write("\n")
+    fr.write(brand)
+    fr.write("\n")
+    fr.write(now1)
+    fr.write("\n")
+    count = 0
+    for line in lines:
+        if re.findall(NullPointer, line) or re.findall(IllegalState, line) or re.findall(IllegalArgument, line) or\
+                re.findall(ArrayIndexOutOfBounds, line) or re.findall(RuntimeException, line) or \
+                re.findall(SecurityException,line):
+            a = lines.index(line)
+            count += 1
+
+    for var in range(a, a+22):
+        print lines[var]
+        fr.write(lines[var])
+        fr.write("\n")
+        f.close()
+        fr.close()
+    return count
+
+number = get_error()
+print number
