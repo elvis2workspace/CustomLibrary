db91e98770ddaf99b5a9c313244256817f56d49b
Elvis Zhang <xiuhai5052@hotmail.com>
2016-09-03 18:08:04 +0800

    add test_algorithm.py and test_monkey.py

---
+++ utils/test_interivew.py
@@ -0,0 +1,108 @@
+# -*- coding: utf-8 -*-
+
+# delete the same element in a list
+
+l = ['b', 'c', 'd', 'b', 'c', 'a', 'a']
+
+# use set
+list(set(l))
+
+# use dictionary
+l2 = {}.fromkeys(l).keys()
+print l2
+
+# 用字典并保持顺序
+l1 = ['b', 'c', 'd', 'b', 'c', 'a', 'a']
+l2 = list(set(l1))
+l2.sort(key=l1.index)
+print l2
+
+
+# 列表推导式
+l1 = ['b', 'c', 'd', 'b', 'c', 'a', 'a']
+l2 = []
+[l2.append(i) for i in l1 if i not in l2]
+print l2
+
+# 6 链表成对调换
+# 1->2->3->4转换成2->1->4->3.
+
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    # @param a ListNode
+    # @return a ListNode
+    def swap_pairs(self, head):
+        if head != None and head.next != None:
+            next_e = head.next
+            head.next = self.swap_pairs(next_e.next)
+            next_e.next = head
+            return next_e
+        return head
+
+l_tmp = [1, 2, 3, 4]
+
+for i in l_tmp:
+    ln_tmp = ListNode(i)
+    solution = Solution()
+    re_list = solution.swap_pairs(ln_tmp)
+    print re_list.val,
+
+# 7 创建字典的方法
+# 1 直接创建
+dict = {'name':'earth', 'port':'80'}
+
+# 2 工厂方法
+items=[('name','earth'), ('port','80')]
+dict2=dict(items)
+dict1=dict((['name','earth'],['port','80']))
+
+# 3 fromkeys()方法
+dict1={}.fromkeys(('x', 'y'), -1)
+dict={'x': -1, 'y': -1}
+dict2={}.fromkeys(('x', 'y'))
+dict2={'x': None, 'y': None}
+
+# 8 合并两个有序列表
+# 知乎远程面试要求编程
+#
+# 尾递归
+
+
+def _recursion_merge_sort2(l1, l2, tmp):
+    if len(l1) == 0 or len(l2) == 0:
+        tmp.extend(l1)
+        tmp.extend(l2)
+        return tmp
+    else:
+        if l1[0] < l2[0]:
+            tmp.append(l1[0])
+            del l1[0]
+        else:
+            tmp.append(l2[0])
+            del l2[0]
+        return _recursion_merge_sort2(l1,l2,tmp)
+
+
+def recursion_merge_sort2(l1, l2):
+    return _recursion_merge_sort2(l1, l2, [])
+
+
+# 循环算法
+def loop_merge_sort(l1,l2):
+    tmp = []
+    while len(l1) > 0 and len(l2) > 0:
+        if l1[0] < l2[0]:
+            tmp.append(l1[0])
+            del l1[0]
+        else:
+            tmp.append(l2[0])
+            del l2[0]
+    tmp.extend(l1)
+    tmp.extend(l2)
+    return tmp
